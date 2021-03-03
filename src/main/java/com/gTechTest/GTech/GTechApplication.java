90331e5e7e486542a7c0bff7f782a83ec8bceef6
bangdolla08 <bangdolla08@gmail.com>
2021-03-03 22:36:28 +0700

    Initial commit

---
+++ src/main/java/com/gTechTest/GTech/GTechApplication.java
@@ -0,0 +1,13 @@
+package com.gTechTest.GTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gTechTest/GTech/GTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gTechTest.GTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
